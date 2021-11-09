79dd91f0343f0c0b362aa54879d32411fc84b454
Uriel <urielenrique99@gmail.com>
2021-11-09 17:07:19 -0600

    cambiado : se agrego un scroll view

--- lib/src/poke_vw.dart
+++ lib/src/poke_vw.dart
@@ -31,150 +31,155 @@ class _PokeViewState extends State<PokeView> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     //FutureBuilder permite construir interfaces a partir del estado de una petición o un evento asincrono
-    return FutureBuilder<PokemonInfo>(
-        future: pokemon,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Column(
-              children: [
-                Container(
-                  width: double.infinity,
-                  padding: EdgeInsets.symmetric(horizontal: 20.0),
-                  decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(50),
-                          bottomRight: Radius.circular(50))),
-                  child: Column(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.symmetric(vertical: 20.0),
-                        height: size.width * 0.7,
-                        child: Stack(
-                          //alignment: Alignment.bottomCenter,
-                          children: [
-                            Container(
-                              height: size.width * 0.6,
-                              width: size.width * 0.6,
-                              decoration: BoxDecoration(
-                                  color: Colors.white, shape: BoxShape.circle),
-                            ),
-                            Image(
-                              image: NetworkImage(
-                                  "${PokemonInfo.imgUrl}/${snapshot.data!.id}.png"),
-                              width: size.width * 0.6,
-                              height: size.width * 0.6,
-                              fit: BoxFit.fill,
-                            )
-                          ],
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                                vertical: 20.0 / 2.5),
-                            child: Text(
-                              'Nombre: ${snapshot.data!.name}',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 30.0,
-                                  color: Colors.black),
-                            ),
-                          ),
-                          RichText(
-                            text: TextSpan(children: [
-                              TextSpan(
-                                  text: 'Peso: ',
-                                  style: Theme.of(context).textTheme.headline6),
-                              TextSpan(
-                                  text: '${snapshot.data!.weight} kg    ',
-                                  style: TextStyle(
-                                      color: Colors.black, fontSize: 16.0)),
-                              TextSpan(
-                                  text: 'Altura: ',
-                                  style: Theme.of(context).textTheme.headline6),
-                              TextSpan(
-                                  text: '${snapshot.data!.height} m',
-                                  style: TextStyle(
-                                      color: Colors.black, fontSize: 16.0)),
-                            ]),
-                          ),
-                          SizedBox(
-                            height: 10.0,
-                          ),
-                          Text(
-                            'Tipos: ',
-                            style: Theme.of(context).textTheme.headline6,
-                          ),
-                          Container(
-                            height: 50.0,
-                            child: ListView.builder(
-                              itemCount: snapshot.data!.types.length,
-                              padding: const EdgeInsets.all(10.0),
-                              itemBuilder: (context, position) {
-                                return RichText(
-                                  text: TextSpan(children: [
-                                    WidgetSpan(
-                                        child: Icon(
-                                      Icons.arrow_forward_rounded,
-                                      size: 15.0,
-                                    )),
-                                    TextSpan(
-                                        text:
-                                            "${snapshot.data!.types[position].type.name}",
-                                        style: TextStyle(
-                                            fontSize: 17.0,
-                                            color: Colors.black))
-                                  ]),
-                                );
-                              },
-                            ),
-                          ),
-                          SizedBox(
-                            height: 20.0,
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-                GestureDetector(
-                  child: Container(
-                    margin: EdgeInsets.all(20.0),
-                    padding: EdgeInsets.symmetric(
-                        horizontal: 20.0, vertical: 20.0 / 2),
+    return SingleChildScrollView(
+      child: FutureBuilder<PokemonInfo>(
+          future: pokemon,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.symmetric(horizontal: 20.0),
                     decoration: BoxDecoration(
                         color: Colors.red,
-                        borderRadius: BorderRadius.circular(30.0)),
-                    child: Row(
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(50),
+                            bottomRight: Radius.circular(50))),
+                    child: Column(
                       children: [
-                        SvgPicture.asset(
-                          "assets/Pokeball.svg",
-                          height: 50.0,
+                        Container(
+                          margin: EdgeInsets.symmetric(vertical: 20.0),
+                          height: size.width * 0.7,
+                          child: Stack(
+                            //alignment: Alignment.bottomCenter,
+                            children: [
+                              Container(
+                                height: size.width * 0.6,
+                                width: size.width * 0.6,
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    shape: BoxShape.circle),
+                              ),
+                              Image(
+                                image: NetworkImage(
+                                    "${PokemonInfo.imgUrl}/${snapshot.data!.id}.png"),
+                                width: size.width * 0.6,
+                                height: size.width * 0.6,
+                                fit: BoxFit.fill,
+                              )
+                            ],
+                          ),
                         ),
-                        Spacer(),
-                        Text(
-                          "Mostrar Pokemón",
-                          style: TextStyle(fontSize: 16.0),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 20.0 / 2.5),
+                              child: Text(
+                                'Nombre: ${snapshot.data!.name}',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 30.0,
+                                    color: Colors.black),
+                              ),
+                            ),
+                            RichText(
+                              text: TextSpan(children: [
+                                TextSpan(
+                                    text: 'Peso: ',
+                                    style:
+                                        Theme.of(context).textTheme.headline6),
+                                TextSpan(
+                                    text: '${snapshot.data!.weight} kg    ',
+                                    style: TextStyle(
+                                        color: Colors.black, fontSize: 16.0)),
+                                TextSpan(
+                                    text: 'Altura: ',
+                                    style:
+                                        Theme.of(context).textTheme.headline6),
+                                TextSpan(
+                                    text: '${snapshot.data!.height} m',
+                                    style: TextStyle(
+                                        color: Colors.black, fontSize: 16.0)),
+                              ]),
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Text(
+                              'Tipos: ',
+                              style: Theme.of(context).textTheme.headline6,
+                            ),
+                            Container(
+                              height: 50.0,
+                              child: ListView.builder(
+                                itemCount: snapshot.data!.types.length,
+                                padding: const EdgeInsets.all(10.0),
+                                itemBuilder: (context, position) {
+                                  return RichText(
+                                    text: TextSpan(children: [
+                                      WidgetSpan(
+                                          child: Icon(
+                                        Icons.arrow_forward_rounded,
+                                        size: 15.0,
+                                      )),
+                                      TextSpan(
+                                          text:
+                                              "${snapshot.data!.types[position].type.name}",
+                                          style: TextStyle(
+                                              fontSize: 17.0,
+                                              color: Colors.black))
+                                    ]),
+                                  );
+                                },
+                              ),
+                            ),
+                            SizedBox(
+                              height: 20.0,
+                            )
+                          ],
                         )
                       ],
                     ),
                   ),
-                  onTap: () {
-                    setState(() {
-                      pokemon = traerPokemon();
-                    });
-                  },
-                )
-              ],
-            );
-          } else if (snapshot.hasError) {
-            return Text('${snapshot.error}');
-          }
+                  GestureDetector(
+                    child: Container(
+                      margin: EdgeInsets.all(20.0),
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 20.0, vertical: 20.0 / 2),
+                      decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(30.0)),
+                      child: Row(
+                        children: [
+                          SvgPicture.asset(
+                            "assets/Pokeball.svg",
+                            height: 50.0,
+                          ),
+                          Spacer(),
+                          Text(
+                            "Mostrar Pokemón",
+                            style: TextStyle(fontSize: 16.0),
+                          )
+                        ],
+                      ),
+                    ),
+                    onTap: () {
+                      setState(() {
+                        pokemon = traerPokemon();
+                      });
+                    },
+                  )
+                ],
+              );
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
 
-          return const CircularProgressIndicator();
-        });
+            return const CircularProgressIndicator();
+          }),
+    );
   }
 }
